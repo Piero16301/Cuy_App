b0cd51f17ecd3b3886c1812611eee183d927e3d8
Piero Angelo Morales Alcalde <piero.morales@utec.edu.pe>
2023-03-31 17:49:57 -0500

    Inicio del proyecto

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.cuy_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:cuy_app/app/app.dart';
+import 'package:cuy_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
